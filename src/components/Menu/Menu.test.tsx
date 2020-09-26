ccf54a7c621340ec8c5c7f3bff1112678d63593d
georgekaran <georgekaran12@gmail.com>
2020-09-26 19:22:53 -0300

    test: ensure Menu closes mobile menu on click

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -28,4 +28,20 @@ describe('<Menu />', () => {
     })
     expect(screen.getByLabelText(/close menu/i)).toBeInTheDocument()
   })
+
+  test('shoud close mobile menu', () => {
+    makeSut()
+    const menuIcon = screen.getByLabelText(/open menu/i)
+    fireEvent.click(menuIcon)
+    const closeIcon = screen.getByLabelText(/close menu/i)
+    fireEvent.click(closeIcon)
+
+    const menuFull = screen.getByRole('navigation', { hidden: true })
+
+    expect(menuFull).toHaveStyle({
+      opacity: '0',
+      pointerEvents: 'none'
+    })
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+  })
 })
