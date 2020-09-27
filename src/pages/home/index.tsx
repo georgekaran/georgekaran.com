3dfdb4b88de90988d743e57b8ac0536546cea023
georgekaran <georgekaran12@gmail.com>
2020-09-27 18:41:32 -0300

    feat: add HighlightBox in Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import * as S from './styles'
+import { HighlightBox } from '@/components/HighlightBox'
+import { Heading } from '@/components/Heading'
 
 const Home: React.FC = () => {
   return (
@@ -17,9 +19,19 @@ const Home: React.FC = () => {
         </S.Description>
       </S.InfoGroup>
 
+      <S.HighlightGroup>
+        <HighlightBox primaryText="2" secondaryText="anos de experiência" />
+        <HighlightBox primaryText="8" secondaryText="projetos desenvolvidos" />
+        <HighlightBox primaryText="+10" secondaryText="cursos concluídos" />
+      </S.HighlightGroup>
+
       <S.MediaGroup>
         <S.Photo src="/img/personal-photo.png" alt="George photo facing the camera"/>
       </S.MediaGroup>
+
+      <S.AboutMe>
+        <Heading>Sobre mim</Heading>
+      </S.AboutMe>
     </S.Wrapper>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,23 +1,37 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+import * as HB from '@/components/HighlightBox/HighlightBox.styles'
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
     padding: ${theme.spacings.xxlarge} 0;
-    display: flex;
+    display: grid;
+    grid-template-columns: 50% 50%;
+    grid-template-areas: "info media" "highlight media";
+
+    ${media.lessThan('medium')`
+      grid-template-areas: "info" "media" "highlight";
+      grid-template-columns: 1fr;
+      padding: 0 0;
+    `}
   `}
 `
 
 export const InfoGroup = styled.section`
   ${({ theme }) => css`
-    flex: 1;
+    grid-area: info;
   `}
 `
 
 export const MediaGroup = styled.section`
   ${({ theme }) => css`
-    flex: 1;
-    display: flex;
-    justify-content: center;
+    grid-area: media;
+    justify-self: center;
+
+    ${media.lessThan('medium')`
+      width: 20em;
+    `}
   `}
 `
 
@@ -28,6 +42,10 @@ export const Job = styled.h4`
     font-weight: ${theme.font.medium};
     margin-bottom: ${theme.spacings.small};
     margin-top: ${theme.spacings.xxlarge};
+
+    ${media.lessThan('medium')`
+      margin-top: ${theme.spacings.medium};
+    `}
   `}
 `
 
@@ -49,4 +67,27 @@ export const Description = styled.h3`
 `
 
 export const Photo = styled.img`
+  width: 100%
+`
+
+export const HighlightGroup = styled.div`
+  ${({ theme }) => css`
+    margin: ${theme.spacings.xlarge} 0;
+    grid-area: highlight;
+    place-self: center;
+
+    ${media.lessThan('medium')`
+      margin: ${theme.spacings.xsmall} 0;
+    `}
+
+    ${HB.Wrapper} {
+      margin-top: ${theme.spacings.medium};
+    }
+  `}
+`
+
+export const AboutMe = styled.section`
+  ${({ theme }) => css`
+    margin-top: ${theme.spacings.xxlarge};
+  `}
 `
