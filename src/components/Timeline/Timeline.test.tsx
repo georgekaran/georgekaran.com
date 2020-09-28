c0deebc5dcf89c038c5fd466b244a5951de75842
georgekaran <georgekaran12@gmail.com>
2020-09-28 16:54:04 -0300

    test: ensure Timeline renders with correct initial state

--- src/components/Timeline/Timeline.test.tsx
+++ src/components/Timeline/Timeline.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { screen } from '@testing-library/react'
 
 import { Timeline, Achievement } from '.'
 import { renderWithTheme } from '@/test/helpers'
@@ -6,38 +7,33 @@ import { renderWithTheme } from '@/test/helpers'
 const achievements: Achievement[] = [
   {
     date: new Date('2020-08-21T00:00:00'),
-    title: 'Node JS com microservicos',
+    title: 'Node JS com microservicos 1',
     description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
     tag: 'education'
   },
   {
     date: new Date('2019-08-21T00:00:00'),
-    title: 'Node JS com microservicos',
+    title: 'Node JS com microservicos 2',
     description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
     tag: 'event'
   },
   {
     date: new Date('2019-08-22T00:00:00'),
-    title: 'Node JS com microservicos',
+    title: 'Node JS com microservicos 3',
     description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
     tag: 'professional'
-  },
-  {
-    date: new Date('2018-08-21T00:00:00'),
-    title: 'Node JS com microservicos',
-    description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
-    tag: 'event'
-  },
-  {
-    date: new Date('2018-08-21T00:00:00'),
-    title: 'Node JS com microservicos',
-    description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
-    tag: 'education'
   }
 ]
 
 describe('<Timeline />', () => {
   it('should render with initial state', () => {
     renderWithTheme(<Timeline achievements={achievements} />)
+    expect(screen.getByText('2020')).toBeInTheDocument()
+    expect(screen.getByText('2019')).toBeInTheDocument()
+
+    expect(screen.getByTestId('events-2020').childElementCount).toBe(1)
+    expect(screen.getByTestId('events-2019').childElementCount).toBe(2)
+
+    expect(screen.queryByText('Role para mais eventos')).not.toBeInTheDocument()
   })
 })
