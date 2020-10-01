f1121f15b1a534d208f6551944168e4a1ac4925d
georgekaran <georgekaran12@gmail.com>
2020-09-30 23:24:02 -0300

    refactor: move sut creation to a factory method

--- src/components/Space/Space.test.tsx
+++ src/components/Space/Space.test.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { screen } from '@testing-library/react'
+import { RenderResult, screen } from '@testing-library/react'
 
 import { Space } from '.'
 import { renderWithTheme } from '@/test/helpers'
 
+const makeSut = (): RenderResult => {
+  return renderWithTheme(<Space />)
+}
+
 describe('<Space />', () => {
   it('should render with initial state', () => {
-    renderWithTheme(<Space />)
+    makeSut()
     expect(screen.getByTestId('fog-1')).toBeInTheDocument()
     expect(screen.getByTestId('fog-2')).toBeInTheDocument()
   })
