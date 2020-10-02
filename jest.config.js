396dbb04452e7fedcfbded0eb9ec8809879d3783
georgekaran <georgekaran12@gmail.com>
2020-10-02 09:17:44 -0300

    build: ignore styles folder from test coverage

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,8 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
     '!**/*.d.ts',
-    '!<rootDir>/src/**/*.stories.tsx'
+    '!<rootDir>/src/**/*.stories.tsx',
+    '!<rootDir>/src/styles/*'
   ],
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
