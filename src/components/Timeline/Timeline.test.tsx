c4a96c56296df011fd2e62f5909472bdb1fd41e7
georgekaran <georgekaran12@gmail.com>
2020-10-02 09:53:04 -0300

    refactor: create achievements factory for Timeline

--- src/components/Timeline/Timeline.test.tsx
+++ src/components/Timeline/Timeline.test.tsx
@@ -4,35 +4,33 @@ import { screen } from '@testing-library/react'
 import { Timeline, Achievement } from '.'
 import { renderWithTheme } from '@/test/helpers'
 
-const achievements = (): Achievement[] => ([
-  {
-    date: new Date('2020-08-21T00:00:00'),
-    title: 'Node JS com microservicos 1',
-    description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
-    tag: 'education'
-  },
-  {
-    date: new Date('2019-08-21T00:00:00'),
-    title: 'Node JS com microservicos 2',
-    description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
-    tag: 'event'
-  },
-  {
-    date: new Date('2019-08-22T00:00:00'),
-    title: 'Node JS com microservicos 3',
-    description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
-    tag: 'professional'
+const createAchievements = (length: number): Achievement[] => {
+  let year = 2018
+  const arr: Achievement[] = []
+  for (let i = 0; i < length; i++) {
+    arr.push(
+      {
+        date: new Date(`${year}-08-21T00:00:00`),
+        title: `Node JS com microservicos ${i}`,
+        description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
+        tag: 'education'
+      }
+    )
+    year = year + 1 > 2020 ? 2018 : year + 1
   }
-])
+  return arr
+}
 
 describe('<Timeline />', () => {
   it('should render with initial state', () => {
-    renderWithTheme(<Timeline achievements={achievements()} />)
-    expect(screen.getByText('2020')).toBeInTheDocument()
-    expect(screen.getByText('2019')).toBeInTheDocument()
+    renderWithTheme(<Timeline achievements={createAchievements(3)} />)
+    expect(screen.getByText('Node JS com microservicos 0')).toBeInTheDocument()
+    expect(screen.getByText('Node JS com microservicos 1')).toBeInTheDocument()
+    expect(screen.getByText('Node JS com microservicos 2')).toBeInTheDocument()
 
     expect(screen.getByTestId('events-2020').childElementCount).toBe(1)
-    expect(screen.getByTestId('events-2019').childElementCount).toBe(2)
+    expect(screen.getByTestId('events-2019').childElementCount).toBe(1)
+    expect(screen.getByTestId('events-2018').childElementCount).toBe(1)
 
     expect(screen.queryByText('Role para mais eventos')).not.toBeInTheDocument()
   })
@@ -41,6 +39,7 @@ describe('<Timeline />', () => {
     renderWithTheme(<Timeline achievements={[]} />)
     expect(screen.queryByText('2020')).not.toBeInTheDocument()
     expect(screen.queryByText('2019')).not.toBeInTheDocument()
+    expect(screen.queryByText('2018')).not.toBeInTheDocument()
 
     expect(screen.queryByText('Role para mais eventos')).not.toBeInTheDocument()
   })
