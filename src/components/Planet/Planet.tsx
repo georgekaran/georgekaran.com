9ae775ba8491c0b25722364ee3134bbf4b4155c7
georgekaran <georgekaran12@gmail.com>
2020-10-03 17:19:17 -0300

    fix: change Planet svg ids to unique

--- src/components/Planet/Planet.tsx
+++ src/components/Planet/Planet.tsx
@@ -13,8 +13,8 @@ const Planet = ({ size = 'medium', orbitElements = [] }: PlanetProps) => (
   <S.Wrapper size={size}>
     <S.PlanetWrapper data-testid="planet">
       <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 560 560">
-        <g filter="url(#filter0_i)">
-          <circle cx="279.815" cy="279.5" r="259.5" fill="url(#paint0_linear)"/>
+        <g filter="url(#filter_full)">
+          <circle cx="279.815" cy="279.5" r="259.5" fill="url(#paint_full_linear)"/>
         </g>
         <circle cx="279.815" cy="279.5" r="269.5" stroke="#0BFFB6" strokeOpacity=".03" strokeWidth="20" strokeMiterlimit="1" strokeDasharray="1 1"/>
         <path d="M272.721 7.709c1.266-.945 23.624-.945 24.468 0 .844.945 1.16 8.428 7.594 13.233 6.433 4.804-47.671 5.985-39.655 0 8.015-5.986 6.328-12.288 7.593-13.233z" fill="#AFEFDD"/>
@@ -63,11 +63,11 @@ const Planet = ({ size = 'medium', orbitElements = [] }: PlanetProps) => (
         <ellipse cx="193.816" cy="100" rx="53.5" ry="9" transform="rotate(-31.063 193.816 100)" fill="#fff" fillOpacity=".7"/>
         <circle cx="132.669" cy="138.353" r="9" transform="rotate(-31.063 132.669 138.353)" fill="#fff" fillOpacity=".7"/>
         <defs>
-          <linearGradient id="paint0_linear" x1="352.183" y1="421.311" x2="345.654" y2="-170.747" gradientUnits="userSpaceOnUse">
+          <linearGradient id="paint_full_linear" x1="352.183" y1="421.311" x2="345.654" y2="-170.747" gradientUnits="userSpaceOnUse">
             <stop stopColor="#1BE0AB"/>
             <stop offset="1" stopColor="#fff"/>
           </linearGradient>
-          <filter id="filter0_i" x=".315" y="0" width="563" height="563" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <filter id="filter_full" x=".315" y="0" width="563" height="563" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
             <feFlood floodOpacity="0" result="BackgroundImageFix"/>
             <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
             <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
@@ -81,12 +81,12 @@ const Planet = ({ size = 'medium', orbitElements = [] }: PlanetProps) => (
         </defs>
       </svg>
       <svg className="front" viewBox="0 0 540 539" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="-40" y="-43" width="577" height="410">
+        <mask id="mask_front" mask-type="alpha" maskUnits="userSpaceOnUse" x="-40" y="-43" width="577" height="410">
           <rect x="-39.5439" y="91.8066" width="520.644" height="283.921" transform="rotate(-15 -39.5439 91.8066)" fill="#C4C4C4"/>
         </mask>
-        <g mask="url(#mask0)">
-          <g filter="url(#filter0_i)">
-            <circle cx="279.815" cy="279.5" r="259.5" fill="url(#paint0_linear)"/>
+        <g mask="url(#mask_front)">
+          <g filter="url(#filter_front)">
+            <circle cx="279.815" cy="279.5" r="259.5" fill="url(#paint_front_linear)"/>
           </g>
           <circle cx="279.815" cy="279.5" r="269.5" stroke="#0BFFB6" strokeOpacity="0.03" strokeWidth="20" strokeMiterlimit="1" strokeDasharray="1 1"/>
           <path d="M272.721 7.70889C273.987 6.76371 296.345 6.76369 297.189 7.70889C298.033 8.65409 298.349 16.1368 304.783 20.9415C311.216 25.7462 257.112 26.9274 265.128 20.9415C273.143 14.9555 271.456 8.65407 272.721 7.70889Z" fill="#AFEFDD"/>
@@ -114,7 +114,7 @@ const Planet = ({ size = 'medium', orbitElements = [] }: PlanetProps) => (
           <circle cx="132.669" cy="138.353" r="9" transform="rotate(-31.0629 132.669 138.353)" fill="white" fillOpacity="0.7"/>
         </g>
         <defs>
-          <filter id="filter0_i" x="0.31543" y="0" width="563" height="563" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <filter id="filter_front" x="0.31543" y="0" width="563" height="563" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
             <feFlood floodOpacity="0" result="BackgroundImageFix"/>
             <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
             <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
@@ -125,7 +125,7 @@ const Planet = ({ size = 'medium', orbitElements = [] }: PlanetProps) => (
             <feColorMatrix type="matrix" values="0 0 0 0 0.189063 0 0 0 0 0.458333 0 0 0 0 0.329083 0 0 0 0.45 0"/>
             <feBlend mode="normal" in2="shape" result="effect1_innerShadow"/>
           </filter>
-          <linearGradient id="paint0_linear" x1="352.183" y1="421.311" x2="345.654" y2="-170.747" gradientUnits="userSpaceOnUse">
+          <linearGradient id="paint_front_linear" x1="352.183" y1="421.311" x2="345.654" y2="-170.747" gradientUnits="userSpaceOnUse">
             <stop stopColor="#1BE0AB"/>
             <stop offset="1" stopColor="white"/>
           </linearGradient>
@@ -134,9 +134,9 @@ const Planet = ({ size = 'medium', orbitElements = [] }: PlanetProps) => (
     </S.PlanetWrapper>
     <S.OnionWrapper data-testid="onion">
       <svg className="onion" viewBox="0 0 1264 872" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1154.01 213.942C1162.18 233.176 1160.81 258.649 1145.36 291.085C1129.93 323.485 1101.65 360.01 1061.44 398.227C981.156 474.525 857.315 553.774 708.8 616.846C560.284 679.918 417.262 713.999 306.61 718.788C251.185 721.187 205.265 716.172 171.236 704.777C137.168 693.37 117.886 676.667 109.718 657.433C101.549 638.199 102.92 612.726 118.369 580.29C133.799 547.89 162.077 511.365 202.291 473.148C282.575 396.85 406.415 317.601 554.931 254.529C703.447 191.457 846.469 157.376 957.121 152.587C1012.55 150.188 1058.47 155.203 1092.49 166.598C1126.56 178.005 1145.84 194.708 1154.01 213.942Z" stroke="url(#paint0_linear)" strokeWidth="50"/>
+        <path d="M1154.01 213.942C1162.18 233.176 1160.81 258.649 1145.36 291.085C1129.93 323.485 1101.65 360.01 1061.44 398.227C981.156 474.525 857.315 553.774 708.8 616.846C560.284 679.918 417.262 713.999 306.61 718.788C251.185 721.187 205.265 716.172 171.236 704.777C137.168 693.37 117.886 676.667 109.718 657.433C101.549 638.199 102.92 612.726 118.369 580.29C133.799 547.89 162.077 511.365 202.291 473.148C282.575 396.85 406.415 317.601 554.931 254.529C703.447 191.457 846.469 157.376 957.121 152.587C1012.55 150.188 1058.47 155.203 1092.49 166.598C1126.56 178.005 1145.84 194.708 1154.01 213.942Z" stroke="url(#paint_ring_linear)" strokeWidth="50"/>
         <defs>
-          <linearGradient id="paint0_linear" x1="545.159" y1="231.518" x2="718.572" y2="639.857" gradientUnits="userSpaceOnUse">
+          <linearGradient id="paint_ring_linear" x1="545.159" y1="231.518" x2="718.572" y2="639.857" gradientUnits="userSpaceOnUse">
             <stop stopColor="#7443FE"/>
             <stop offset="0.484375" stopColor="#DBFFF5"/>
             <stop offset="1" stopColor="#129D5A"/>
