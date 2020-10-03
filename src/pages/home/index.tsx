12e9e84c37cd60152db116230ad2799441ce9004
georgekaran <georgekaran12@gmail.com>
2020-10-03 14:18:39 -0300

    feat: add section about me

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,40 @@ import React from 'react'
 import * as S from './styles'
 import { HighlightBox } from '@/components/HighlightBox'
 import { Heading } from '@/components/Heading'
+import { Achievement, Timeline } from '@/components/Timeline'
+
+const achievements: Achievement[] = [
+  {
+    date: new Date('2020-08-21T00:00:00'),
+    title: 'Node JS com microservicos',
+    description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
+    tag: 'education'
+  },
+  {
+    date: new Date('2019-08-21T00:00:00'),
+    title: 'Node JS com microservicos',
+    description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
+    tag: 'event'
+  },
+  {
+    date: new Date('2019-08-22T00:00:00'),
+    title: 'Node JS com microservicos',
+    description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
+    tag: 'professional'
+  },
+  {
+    date: new Date('2018-08-21T00:00:00'),
+    title: 'Node JS com microservicos',
+    description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
+    tag: 'event'
+  },
+  {
+    date: new Date('2018-08-21T00:00:00'),
+    title: 'Node JS com microservicos',
+    description: 'Node JS com microservicos Node JS com microservicos Node JS com microservicosNode JS com microservicos Node JS com microservicos Node JS com microservicos Node JS com microservicos',
+    tag: 'education'
+  }
+]
 
 const Home: React.FC = () => {
   return (
@@ -31,6 +65,29 @@ const Home: React.FC = () => {
 
       <S.AboutMe>
         <Heading>Sobre mim</Heading>
+
+        <S.AboutMeContentWrapper>
+          <S.History>
+            <S.Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies amet orci a pulvinar et cursus.
+              Pulvinar in nulla arcu duis hendrerit eget amet.
+              Turpis pellentesque pulvinar eu cras a.
+              In viverra curabitur vitae id. Tellus eleifend habitant sem congue.
+              Eget gravida tincidunt sollicitudin luctus non tincidunt. Volutpat eget enim quis rhoncus placerat lectus.
+              Morbi amet congue risus curabitur vitae amet, eu posuere turpis. Porttitor lorem pretium quis felis donec sed nulla tristique.
+            </S.Paragraph>
+
+            <S.Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies amet orci a pulvinar et cursus.
+              Pulvinar in nulla arcu duis hendrerit eget amet.
+              Turpis pellentesque pulvinar eu cras a. In viverra curabitur vitae id.
+            </S.Paragraph>
+          </S.History>
+
+          <S.TimelineWrapper>
+            <Timeline achievements={achievements} />
+          </S.TimelineWrapper>
+        </S.AboutMeContentWrapper>
       </S.AboutMe>
     </S.Wrapper>
   )
