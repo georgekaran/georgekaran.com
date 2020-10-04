b4d536f24c7fb2a10ce1a484dddfddec58864bb0
georgekaran <georgekaran12@gmail.com>
2020-10-03 23:49:04 -0300

    test: ensure Space render children on the dom

--- src/components/Space/Space.test.tsx
+++ src/components/Space/Space.test.tsx
@@ -18,6 +18,11 @@ describe('<Space />', () => {
     expect(screen.getByTestId('fog-2')).toBeInTheDocument()
   })
 
+  it('should render children on the dom', () => {
+    makeSut({ children: <h1>any_content</h1> })
+    expect(screen.getByRole('heading', { name: /any_content/ })).toBeInTheDocument()
+  })
+
   it('should generate the amount of stars provided', () => {
     makeSut({ stars: 10 })
     const stars = screen.getAllByTestId('star')
