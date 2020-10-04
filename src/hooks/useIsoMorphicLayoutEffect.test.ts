337024903fa3de885d1cfea8dd8d016fce440c6c
georgekaran <georgekaran12@gmail.com>
2020-10-04 00:38:35 -0300

    refactor: move sut creation to a factory method

--- src/hooks/useIsoMorphicLayoutEffect.test.ts
+++ src/hooks/useIsoMorphicLayoutEffect.test.ts
@@ -1,17 +1,21 @@
 import { useEffect, useLayoutEffect } from 'react'
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, RenderHookResult } from '@testing-library/react-hooks'
 
 import useIsoMorphicLayoutEffect from './useIsoMorphicLayoutEffect'
+
+const makeSut = (window: globalThis.Window): RenderHookResult<any, any> => {
+  return renderHook(() => useIsoMorphicLayoutEffect(window))
+}
 
 describe('useIsoMorphicLayoutEffect', () => {
   it('should return useEffect if typeof window is undefined', () => {
     // @ts-ignore
-    const { result } = renderHook(() => useIsoMorphicLayoutEffect(undefined))
+    const { result } = makeSut(undefined)
     expect(result.current).toEqual(useEffect)
   })
 
   it('should return useEffect if typeof window is undefined', () => {
-    const { result } = renderHook(() => useIsoMorphicLayoutEffect(global.window))
+    const { result } = makeSut(global.window)
     expect(result.current).toEqual(useLayoutEffect)
   })
 })
