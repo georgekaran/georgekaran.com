5243c59414d6670239f1620c8a71e1332ea79e5f
georgekaran <georgekaran12@gmail.com>
2020-10-05 21:50:01 -0300

    refactor: move sut creation to a factory method

--- src/components/MediaMatch/MediaMatch.test.tsx
+++ src/components/MediaMatch/MediaMatch.test.tsx
@@ -1,38 +1,45 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, RenderResult, screen } from '@testing-library/react'
 
 import { MediaMatch } from '.'
 
+type SutTypes = {
+  sut: RenderResult
+  desktopHeading: Element
+  mobileHeading: Element
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(
+    <>
+      <MediaMatch greaterThan="medium">
+        <h1 data-testid="desktop">Desktop</h1>
+      </MediaMatch>
+      <MediaMatch lessThan="medium">
+        <h1 data-testid="mobile">Mobile</h1>
+      </MediaMatch>
+    </>
+  )
+
+  return {
+    sut,
+    desktopHeading: screen.getByTestId('desktop'),
+    mobileHeading: screen.getByTestId('mobile')
+  }
+}
+
 describe('<MediaMatch />', () => {
-  let desktopHeading: Element
-  let mobileHeading: Element
-
-  beforeEach(() => {
-    render(
-      <>
-        <MediaMatch greaterThan="medium">
-          <h1 data-testid="desktop">Desktop</h1>
-        </MediaMatch>
-        <MediaMatch lessThan="medium">
-          <h1 data-testid="mobile">Mobile</h1>
-        </MediaMatch>
-      </>
-    )
-
-    desktopHeading = screen.getByTestId('desktop')
-    mobileHeading = screen.getByTestId('mobile')
-  })
-
   test('should be hidden if no media query is passed', () => {
+    const { desktopHeading, mobileHeading } = makeSut()
     expect(desktopHeading.parentElement).toHaveStyleRule('display', 'none')
     expect(mobileHeading.parentElement).toHaveStyleRule('display', 'none')
   })
 
   test('should show or hide content based on the media provided', () => {
+    const { desktopHeading, mobileHeading } = makeSut()
     expect(desktopHeading.parentElement).toHaveStyleRule('display', 'block', {
       media: '(min-width: 768px)'
     })
-
     expect(mobileHeading.parentElement).toHaveStyleRule('display', 'block', {
       media: '(max-width: 768px)'
     })
