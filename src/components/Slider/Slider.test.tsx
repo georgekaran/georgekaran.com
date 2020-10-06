bcc49dce5a002742e0dc974d012dab6f078bfaa4
georgekaran <georgekaran12@gmail.com>
2020-10-06 15:42:49 -0300

    refactor: move sut creation to a factory method

--- src/components/Slider/Slider.test.tsx
+++ src/components/Slider/Slider.test.tsx
@@ -1,18 +1,25 @@
 import '@/test/mock-match-media'
 import React from 'react'
 
-import { Slider } from '.'
+import { Slider, SliderProps } from '.'
 import { renderWithTheme } from '@/test/helpers'
+import { RenderResult } from '@testing-library/react'
+
+type SutProps = Partial<SliderProps>
+
+const makeSut = ({ settings }: SutProps = {}): RenderResult => {
+  return renderWithTheme(
+    <Slider settings={settings}>
+      <h1 key="1">1</h1>
+      <h1 key="2">2</h1>
+      <h1 key="3">3</h1>
+    </Slider>
+  )
+}
 
 describe('<Slider />', () => {
   it('should render with 1 active item', () => {
-    const { container } = renderWithTheme(
-      <Slider>
-        <h1 key="1">1</h1>
-        <h1 key="2">2</h1>
-        <h1 key="3">3</h1>
-      </Slider>
-    )
+    const { container } = makeSut()
     expect(container.querySelectorAll('.slick-slide')).toHaveLength(7)
     expect(container.querySelectorAll('li.slick-active')).toHaveLength(1)
   })
