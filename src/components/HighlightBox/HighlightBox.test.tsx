f2e09cb03be4a03301e7e8efbda18f79c8ebddd5
georgekaran <georgekaran12@gmail.com>
2020-10-08 17:40:57 -0300

    test: ensure HighlightBox changes background on hover

--- src/components/HighlightBox/HighlightBox.test.tsx
+++ src/components/HighlightBox/HighlightBox.test.tsx
@@ -9,12 +9,19 @@ describe('<HighlightBox />', () => {
     renderWithTheme(<HighlightBox primaryText="10" secondaryText="any_text" />)
     expect(screen.getByRole('heading', { name: /10/i })).toBeInTheDocument()
     expect(screen.getByRole('heading', { name: /any_text/i })).toBeInTheDocument()
-    expect(screen.getByTestId('ellipse')).toHaveStyle({
+
+    const ellipse = screen.getByTestId('ellipse')
+    expect(ellipse).toHaveStyle({
       backgroundImage: 'url("data:image/svg+xml,%3csvg width=\'100%25\' height=\'100%25\' xmlns=\'http://www.w3.org/2000/svg\'%3e%3crect width=\'100%25\' height=\'100%25\' fill=\'none\' rx=\'100\' ry=\'100\' stroke=\'%2328F5BEFF\' stroke-width=\'2\' stroke-dasharray=\'8\' stroke-dashoffset=\'71\' stroke-linecap=\'butt\'/%3e%3c/svg%3e")',
       borderRadius: '100px',
       width: '8rem',
       height: '8rem'
     })
+
+    expect(ellipse).toHaveStyleRule(
+      'background', 'linear-gradient(83.89deg,#28F5BE 2.81%,#3B37FF 97.94%)',
+      { modifier: ':hover' }
+    )
   })
 
   it('should match snapshot', () => {
