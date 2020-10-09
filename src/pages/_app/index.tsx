c8e67b8cb3225be8e6f77478d7bf144b37cd12d7
georgekaran <georgekaran12@gmail.com>
2020-10-09 13:01:41 -0300

    feat: update manifest.json with new info

--- src/pages/_app/index.tsx
+++ src/pages/_app/index.tsx
@@ -13,8 +13,8 @@ function App ({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <Head>
         <title>George Karan Personal Page</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="/img/icon.svg" />
+        <link rel="apple-touch-icon" href="/img/icon.svg" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="description" content="This is my personal page where i'll post my projects and updates about me." />
       </Head>
