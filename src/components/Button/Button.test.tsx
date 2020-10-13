7f5f395f14aba245d32155a95600704be9bf7d7e
georgekaran <georgekaran12@gmail.com>
2020-10-13 11:56:30 -0300

    test: ensure Button render as anchor

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { screen, RenderResult } from '@testing-library/react'
 import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
-import { Button, ButtonProps } from '.'
+import { Button, ButtonProps, ButtonTypes } from '.'
 import { renderWithTheme } from '@/test/helpers'
 import theme from '@/styles/theme'
 
-type SutProps = Omit<ButtonProps, 'children'>
+type SutProps = Omit<ButtonProps, 'children'> & ButtonTypes
 
 const makeSut = ({ size = 'medium', fullWidth = false, icon = null, as = 'button', ...props }: SutProps = {}): RenderResult => {
   return renderWithTheme(<Button size={size} fullWidth={fullWidth} icon={icon} as={as} {...props}>Button</Button>)
@@ -49,5 +49,11 @@ describe('<Button />', () => {
     const icon = screen.getByTestId('icon')
     expect(icon).toBeInTheDocument()
     expect(screen.getByText(/Button/)).toBeInTheDocument()
+  })
+
+  test('should render as anchor if prop \'as\' is a', () => {
+    makeSut({ as: 'a', href: '/link' })
+    const buttonAsLink = screen.getByRole('link', { name: /Button/i })
+    expect(buttonAsLink).toBeInTheDocument()
   })
 })
