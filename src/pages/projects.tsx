65615b31d2f2ee8181d1858745af9b0cc99c56f3
georgekaran <georgekaran12@gmail.com>
2020-10-13 23:08:11 -0300

    feat: add ProjectCard to Project page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -3,20 +3,72 @@ import React from 'react'
 import * as S from '@/styles/pages/projects'
 import * as Animations from '@/styles/animations'
 import { Heading } from '@/components/Heading'
+import { ProjectCard } from '@/components/ProjectCard'
 
 const Projects = () => {
   return (
     <S.Wrapper>
       <Heading
         data-aos="slide-right"
-        data-aos-offset="10"
         {...Animations.baseAosAnimation}
-        data-aos-duration="1000"
       >
           Projetos
       </Heading>
+      <S.Subtitle
+        data-aos="slide-right"
+        {...Animations.baseAosAnimation}
+      >
+        Aqui estão alguns projetos que fiz ao longo de minha trajetória, espero que goste!
+      </S.Subtitle>
       <S.ProjectsWrapper>
-        TODO
+        <ProjectCard
+          title='georgekaran.com'
+          tags={['React', 'Next.JS', 'Styled-Components']}
+          image='/img/projects/georgekaran-card.png'
+          description='George Karan personal page where you can find some of my projects and updates about me.'
+          data-aos="fade-up"
+          {...Animations.baseAosAnimation}
+        />
+        <ProjectCard
+          title='Won Games'
+          tags={['React', 'Strapi', 'Typescript']}
+          image='/img/projects/won-games-card.png'
+          description='Ecommerce project for selling games using Strapi, React and Styled-Components.'
+          data-aos="fade-up"
+          {...Animations.baseAosAnimation}
+        />
+        <ProjectCard
+          title='Ticketing'
+          tags={['React', 'NodeJS', 'NATS']}
+          image='/img/projects/ticketing-card.png'
+          description='Buy and sell tickets in this microservices based project.'
+          data-aos="fade-up"
+          {...Animations.baseAosAnimation}
+        />
+        <ProjectCard
+          title='Survey App'
+          tags={['React', 'NodeJS', 'Typescript']}
+          image='/img/projects/survey-card.png'
+          description='Survey project written in React and Typescript using TDD.'
+          data-aos="fade-up"
+          {...Animations.baseAosAnimation}
+        />
+        <ProjectCard
+          title='Ecoleta'
+          tags={['React', 'NodeJS', 'Native']}
+          image='/img/projects/ecoleta-card.png'
+          description='Market place for waste collection written in React, React-Native and Node.JS.'
+          data-aos="fade-up"
+          {...Animations.baseAosAnimation}
+        />
+        <ProjectCard
+          title='Star Wars UI'
+          tags={['React', 'NodeJS', 'Typescript']}
+          image='/img/projects/star-wars-card.png'
+          description='Star Wars UI landing page consuming data from https://swapi.co.'
+          data-aos="fade-up"
+          {...Animations.baseAosAnimation}
+        />
       </S.ProjectsWrapper>
     </S.Wrapper>
   )
