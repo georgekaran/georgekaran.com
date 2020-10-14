186d1d7d80dce6106e841f0748becab07d63e558
georgekaran <georgekaran12@gmail.com>
2020-10-13 23:06:08 -0300

    refactor: move tags place and polish css

--- src/components/ProjectCard/ProjectCard.styles.ts
+++ src/components/ProjectCard/ProjectCard.styles.ts
@@ -6,6 +6,7 @@ export const Wrapper = styled.div`
     background: ${theme.colors.gradient05};
     max-width: 50rem;
     min-width: 28rem;
+    max-height: 686px;
     width: 100%;
     display: grid;
     grid-template-columns: 1fr 92% 1fr;
@@ -24,6 +25,10 @@ export const HeaderWrapper = styled.div`
     justify-content: space-between;
     align-items: center;
     margin-bottom: ${theme.spacings.small};
+
+    ${media.lessThan('medium')`
+      margin-bottom: ${theme.spacings.xsmall};
+    `}
   `}
 `
 
@@ -38,26 +43,34 @@ export const Title = styled.h2`
 `
 
 export const TagWrapper = styled.div`
-  display: flex;
+  ${({ theme }) => css`
+    display: flex;
+    margin-top: ${theme.spacings.small};
+
+    ${media.lessThan('medium')`
+      margin-top: ${theme.spacings.xsmall};
+    `};
+  `}
 `
 
 export const Tag = styled.span`
   ${({ theme }) => css`
-    font-size: ${theme.font.sizes.xsmall};
-    font-weight: ${theme.font.bold};
+    font-size: 1.2rem;
+    font-weight: ${theme.font.medium};
     text-transform: uppercase;
     color: ${theme.colors.white};
-    padding: ${theme.spacings.xxsmall};
+    padding: 0.4rem;
     background: ${theme.colors.purple};
     border-radius: 0.4rem;
-    letter-spacing: 1px;
+    height: 2.6rem;
+    letter-spacing: 0.4px;
 
     &:not(:first-child) {
       margin-left: ${theme.spacings.xsmall};
     }
 
-    ${media.lessThan('small')`
-      display: none;
+    ${media.lessThan('medium')`
+      font-size: 1rem;
     `};
   `}
 `
@@ -84,8 +97,9 @@ export const Description = styled.p`
     font-weight: ${theme.font.normal};
     color: ${theme.colors.gray};
 
-    ${media.lessThan('small')`
+    ${media.lessThan('medium')`
       font-size: 1.2rem;
+      margin: ${theme.spacings.xsmall} 0;
     `};
   `}
 `
--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { HTMLAttributes } from 'react'
 
 import * as S from './ProjectCard.styles'
 import { Button } from '@/components/Button'
@@ -8,17 +8,17 @@ export type ProjectCardProps = {
   tags: string[]
   image: string
   description: string
-}
+} & HTMLAttributes<HTMLDivElement>
 
-const ProjectCard = ({ title, tags, description, image }: ProjectCardProps) => (
-  <S.Wrapper>
+const ProjectCard = ({ title, tags, description, image, ...props }: ProjectCardProps) => (
+  <S.Wrapper {...props}>
     <S.HeaderWrapper>
       <S.Title>{title}</S.Title>
-      <S.TagWrapper>
-        {tags.map(tag => <S.Tag key={tag}>{tag}</S.Tag>)}
-      </S.TagWrapper>
     </S.HeaderWrapper>
     <S.Image src={image} alt={title} />
+    <S.TagWrapper>
+      {tags.map(tag => <S.Tag key={tag}>{tag}</S.Tag>)}
+    </S.TagWrapper>
     <S.Description>
       {description}
     </S.Description>
