e029429960bc0556f938fe7cd84b0c5202413688
georgekaran <georgekaran12@gmail.com>
2020-10-15 17:22:57 -0300

    refactor: move sut creation to a factory method

--- src/components/ContactCard/ContactCard.test.tsx
+++ src/components/ContactCard/ContactCard.test.tsx
@@ -3,17 +3,21 @@ import { Github as GithubIcon } from '@styled-icons/feather/Github'
 
 import { ContactCard } from '.'
 import { renderWithTheme } from '@/test/helpers'
-import { screen } from '@testing-library/react'
+import { RenderResult, screen } from '@testing-library/react'
+
+const makeSut = (): RenderResult => {
+  return renderWithTheme(
+    <ContactCard
+      icon={<GithubIcon aria-label="Github Icon" />}
+      previewLink="georgekaran"
+      fullLink="https://github.com/georgekaran"
+    />
+  )
+}
 
 describe('<ContactCard />', () => {
   it('should render with initial state', () => {
-    renderWithTheme(
-      <ContactCard
-        icon={<GithubIcon aria-label="Github Icon" />}
-        previewLink="georgekaran"
-        fullLink="https://github.com/georgekaran"
-      />
-    )
+    makeSut()
     expect(screen.getByLabelText(/github icon/i)).toBeInTheDocument()
     expect(screen.getByText('georgekaran')).toBeInTheDocument()
     expect(screen.getByTestId('overlay')).toHaveAttribute('aria-hidden', 'true')
