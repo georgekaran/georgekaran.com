dbd5f3d01aaa3c2f450a3e8ca18a5b672f30d734
georgekaran <georgekaran12@gmail.com>
2020-10-18 22:41:43 -0300

    test: ensure CurrentLanguageAdapter calls localStorage.getItem with correct value

--- src/adapters/current-language.spec.ts
+++ src/adapters/current-language.spec.ts
@@ -1,10 +1,18 @@
 import { mockLanguage } from '@/test/mockLanguage'
-import { setCurrentLanguageAdapter } from './current-language'
+import { setCurrentLanguageAdapter, getCurrentLanguageAdapter } from './current-language'
 
 describe('CurrentLanguageAdapter', () => {
   test('Should call localStorage.setItem with correct value', () => {
     const language = mockLanguage()
     setCurrentLanguageAdapter(language)
     expect(localStorage.setItem).toHaveBeenCalledWith('__georgekaran_language', language)
+  })
+
+  test('Should call localStorage.getItem with correct value', () => {
+    const language = mockLanguage()
+    const localStorageGetItemSpy = jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(language)
+    const result = getCurrentLanguageAdapter()
+    expect(localStorageGetItemSpy).toHaveBeenCalledWith('__georgekaran_language')
+    expect(result).toEqual(language)
   })
 })
