7901f669056a4c239569be72a34f24f31593f8a6
georgekaran <georgekaran12@gmail.com>
2020-10-19 16:40:50 -0300

    test: ensure I18N replace message that contain masks correctly

--- src/infra/i18n/i18n.spec.ts
+++ src/infra/i18n/i18n.spec.ts
@@ -5,15 +5,18 @@ import { mockLanguage } from '@/test/mockLanguage'
 const messagesMock = {
   pt_BR: {
     any_msg: 'any_value_pt_BR',
-    any_msg_mask: 'any_$1_pt_BR'
+    any_msg_mask: 'any_$0_pt_BR',
+    any_msg2_mask: 'any_$0_$1_pt_BR'
   },
   en_US: {
     any_msg: 'any_value_en_US',
-    any_msg_mask: 'any_$1_en_US'
+    any_msg_mask: 'any_$0_en_US',
+    any_msg2_mask: 'any_$0_$1_en_US'
   },
   es_ES: {
-    any_msg: 'any_value_en_US',
-    any_msg_mask: 'any_$1_en_US'
+    any_msg: 'any_value_es_ES',
+    any_msg_mask: 'any_$0_es_ES',
+    any_msg2_mask: 'any_$0_$1_es_ES'
   }
 }
 
@@ -37,5 +40,18 @@ describe('i18n', () => {
     const { sut, language } = makeSut()
     expect(sut.language).toEqual(language)
     expect(messagesMock[language].any_msg).toEqual(sut.t('any_msg'))
+  })
+
+  test('should replace message with mask correctly', () => {
+    const { sut, language } = makeSut()
+    const result1 = sut.t('any_msg_mask', ['any_value'])
+    const result2 = sut.t('any_msg2_mask', ['any_value', 'other_value'])
+
+    expect(result1).not.toContain('$0')
+    expect(result2).not.toContain('$0')
+    expect(result2).not.toContain('$1')
+
+    expect(result1).toBe(messagesMock[language].any_msg_mask.replace('$0', 'any_value'))
+    expect(result2).toBe(messagesMock[language].any_msg2_mask.replace('$0', 'any_value').replace('$1', 'other_value'))
   })
 })
