1bb827f788f343fea86797fb57c9429e9eea6e52
georgekaran <georgekaran12@gmail.com>
2020-10-18 23:10:14 -0300

    test: add jest-localstorage-mock to setup.ts

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+import 'jest-localstorage-mock'
 
 // Error: Not implemented: window.scrollTo
 Object.defineProperty(window, 'scrollTo', { value: () => {}, writable: true })
