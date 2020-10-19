009244d130e0dab3ecd7d7dcdbaf7dc744e86835
georgekaran <georgekaran12@gmail.com>
2020-10-18 22:41:35 -0300

    feat: ensure CurrentLanguageAdapter calls localStorage.getItem with correct value

--- src/adapters/current-language.ts
+++ src/adapters/current-language.ts
@@ -3,3 +3,7 @@ import { Language } from '@/domain/models/language'
 export const setCurrentLanguageAdapter = (language: Language): void => {
   localStorage.setItem('__georgekaran_language', language)
 }
+
+export const getCurrentLanguageAdapter = (): Language => {
+  return localStorage.getItem('__georgekaran_language') as Language || 'pt_BR'
+}
