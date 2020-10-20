a03826f0f0872c0cad63b49965e2f939b29cfe5e
georgekaran <georgekaran12@gmail.com>
2020-10-19 22:59:37 -0300

    fix: change mask string to start on 0

--- src/main/config/messages.ts
+++ src/main/config/messages.ts
@@ -3,15 +3,15 @@ import { MessageFile } from '@/data/protocols'
 const messages: MessageFile = {
   pt_BR: {
     copy_to_clipboard: 'Copiado',
-    copy_url: 'Copiar $1 url'
+    copy_url: 'Copiar $0 url'
   },
   en_US: {
     copy_to_clipboard: 'Copied to clipboard',
-    copy_url: 'Copy $1 url'
+    copy_url: 'Copy $0 url'
   },
   es_ES: {
     copy_to_clipboard: 'Copiado',
-    copy_url: 'Copiar url de $1'
+    copy_url: 'Copiar url de $0'
   }
 }
 
