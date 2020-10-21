16c5a593e16941a2bdd5c878d6cc18d56fa3ac9d
georgekaran <georgekaran12@gmail.com>
2020-10-20 22:05:20 -0300

    feat: add new values to messages

--- src/main/config/messages.ts
+++ src/main/config/messages.ts
@@ -2,17 +2,62 @@ import { MessageFile } from '@/data/protocols'
 
 const messages: MessageFile = {
   pt_BR: {
+    home: 'Home',
+    projects: 'Projetos',
+    contact: 'Contato',
+    open_menu: 'Abrir menu',
+    close_menu: 'Fechar menu',
+    planet_label: 'Planeta verde com buracos pretos',
+    planet_ring: 'Anel do Planeta',
+    fullstack: 'Full Stack Engineer',
+    presentation: 'Olá, eu sou George',
+    presentation_description: 'Um desenvolvedor apaixonado pela tecnologia web e amante de conceitos de UI design!',
+    years_experience: 'anos de experiência',
+    projects_developed: 'projetos desenvolvidos',
+    completed_courses: 'cursos concluídos',
+    george_front_alt: 'Minha foto olhando para a frente',
     copy_to_clipboard: 'Copiado',
-    copy_url: 'Copiar $0 url'
+    copy_url: 'Copiar $0 url',
+    open_url: 'Abrir $0'
   },
   en_US: {
+    home: 'Home',
+    projects: 'Projects',
+    contact: 'Contact',
+    open_menu: 'Open menu',
+    close_menu: 'Close menu',
+    planet_label: 'Green planet with dark holes',
+    planet_ring: 'Planet\'s ring',
+    fullstack: 'Full Stack Engineer',
+    presentation: 'Hi, i\'m George',
+    presentation_description: 'A developer passionate about web technology and a lover of UI design concepts!',
+    years_experience: 'years of experience',
+    projects_developed: 'projects developed',
+    completed_courses: 'completed courses',
+    george_front_alt: 'My photo facing forward',
     copy_to_clipboard: 'Copied to clipboard',
-    copy_url: 'Copy $0 url'
+    copy_url: 'Copy $0 url',
+    open_url: 'Open $0'
   },
   es_ES: {
+    home: 'Home',
+    projects: 'Proyectos',
+    contact: 'Contacto',
+    open_menu: 'Abrir menú',
+    close_menu: 'Cerrar menú',
+    planet_label: 'Planeta verde con agujeros oscuros',
+    planet_ring: 'Anillo del planeta',
+    fullstack: 'Ingeniero Full Stack',
+    presentation: 'Hola, soy George',
+    presentation_description: 'Un desarrollador apasionado por la tecnología web y amante de los conceptos de diseño de UI.',
+    years_experience: 'años de experiencia',
+    projects_developed: 'proyectos desarrollados',
+    completed_courses: 'cursos completados',
+    george_front_alt: 'Mi foto mirando hacia adelante',
     copy_to_clipboard: 'Copiado',
-    copy_url: 'Copiar url de $0'
+    copy_url: 'Copiar url de $0',
+    open_url: 'Abrir $0'
   }
-}
+} as const
 
 export default messages
