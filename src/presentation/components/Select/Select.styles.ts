c4ffedef36c9a6a0bc52fe68e4827d5700ec1a40
georgekaran <georgekaran12@gmail.com>
2020-10-23 17:57:51 -0300

    style: resize Select font-size and add more padding left-right

--- src/presentation/components/Select/Select.styles.ts
+++ src/presentation/components/Select/Select.styles.ts
@@ -6,16 +6,15 @@ export const Wrapper = styled.div`
 
 export const Select = styled.select`
   ${({ theme }) => css`
-    padding: ${theme.spacings.xxsmall};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xsmall};
     appearance: none;
     background: transparent;
     background-image: url("data:image/svg+xml;utf8,<svg viewBox='0 0 24 24' width='24' height='24' fill='%2328F5BE' xmlns='http://www.w3.org/2000/svg'><path d='M5.8 9.7L12 16l6.2-6.3c.2-.2.3-.5.3-.7s-.1-.5-.3-.7c-.2-.2-.4-.3-.7-.3h-11c-.3 0-.5.1-.7.3-.2.2-.3.4-.3.7s.1.5.3.7z'></path></svg>");
     background-repeat: no-repeat;
-    background-position-x: calc(100% - 0.8rem);
-    background-position-y: 8px;
+    background-position-x: calc(100% - 1rem);
+    background-position-y: 6px;
     border-radius: 6px;
-    font-size: ${theme.font.sizes.medium};
-    width: calc(100% + 3.2rem);
+    width: 100%;
     border: 1px solid ${theme.colors.gray};
     color: ${theme.colors.white};
 
@@ -27,7 +26,6 @@ export const Select = styled.select`
 
 export const Option = styled.option`
   ${({ theme }) => css`
-    font-size: ${theme.font.sizes.medium};
     color: ${theme.colors.black};
   `}
 `
