47bef0475c2323f11153e4d2daa945d6b9a808a7
georgekaran <georgekaran12@gmail.com>
2020-10-26 22:43:44 -0300

    refactor: remove AOS external js and add aos npm dependency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import Head from 'next/head'
+import AOS from 'aos'
 
 import * as S from '@/presentation/styles/pages/_app'
 import GlobalStyles from '@/presentation/styles/global'
@@ -13,6 +14,10 @@ import I18NProvider from '@/main/factories/context/I18NProvider'
 import { getCurrentLanguageAdapter, setCurrentLanguageAdapter } from '@/main/adapters/current-language'
 
 function App ({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init()
+  }, [])
+
   return (
     <LanguageProvider
       setCurrentLanguage={setCurrentLanguageAdapter}
@@ -26,10 +31,6 @@ function App ({ Component, pageProps }: AppProps) {
             <link rel="apple-touch-icon" href="/img/icon.svg" />
             <link rel="manifest" href="/manifest.json" />
             <link rel="stylesheet" href="/css/aos.css" />
-            <script src="/js/aos.js" async></script>
-            {process.browser && <script async>
-            AOS.init()
-            </script>}
             <meta name="description" content="This is my personal page where i'll post my projects and updates about me." />
           </Head>
           <GlobalStyles />
