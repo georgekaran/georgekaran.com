2d1fbd388ef75a749ad7b4abab77232c7f13ee2a
georgekaran <georgekaran12@gmail.com>
2020-10-30 11:14:06 -0300

    fix: change logo to 'george karan'

--- src/presentation/components/Footer/Footer.test.tsx
+++ src/presentation/components/Footer/Footer.test.tsx
@@ -7,7 +7,7 @@ import { render } from '@/test/helpers'
 describe('<Footer />', () => {
   it('should render with initial state', () => {
     render(<Footer />)
-    expect(screen.getByLabelText(/george mueller/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/george karan/i)).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /email/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /linkedin/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /github/i })).toBeInTheDocument()
