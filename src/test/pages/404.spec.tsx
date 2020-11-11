8ff0b5d6ae30bd25470af8994c20a9436c69a6e6
georgekaran <georgekaran12@gmail.com>
2020-11-11 10:36:45 -0300

    test: ensure 404 page renders with correct data

---
+++ src/test/pages/404.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { screen } from '@testing-library/react'
+
+import { render } from '@/test/helpers'
+import NotFound from '@/pages/404'
+import theme from '@/presentation/styles/theme'
+
+describe('404 Page', () => {
+  it('should render 404 with correct data', () => {
+    render(<NotFound />)
+    expect(screen.getByText('404!')).toHaveStyle({
+      color: theme.colors.green,
+      fontSize: 'min(10rem, 20vw)'
+    })
+    expect(screen.getByText(/Opss, parece que a página/i)).toHaveStyle({
+      color: theme.colors.white,
+      fontSize: 'min(10rem, white)',
+      fontWeight: theme.font.medium,
+      marginBottom: theme.spacings.large,
+      textAlign: 'justify'
+    })
+    expect(screen.getByText(/voltar ao home/i).parentElement).toHaveAttribute('href', '/')
+  })
+})
