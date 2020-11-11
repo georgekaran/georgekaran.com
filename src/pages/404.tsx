60dafa16d12f365f5d130311880c595ec0d7e96b
georgekaran <georgekaran12@gmail.com>
2020-11-11 12:18:29 -0300

    feat: ensure 404 page go to home on button click

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,15 @@
 import * as S from '@/presentation/styles/pages/404'
 import { Space } from '@/presentation/components/Space'
 import { Button } from '@/presentation/components/Button'
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
+  const router = useRouter()
+
+  const redirectToHome = () => {
+    router.push('/')
+  }
+
   return (
     <S.Wrapper>
       <Space stars={30} >
@@ -13,9 +20,8 @@ const NotFound = () => {
           Opss, parece que a página que você tentou acessar não existe!
         </S.Subtitle>
         <Button
-          as="a"
-          href={'/'}
           size="large"
+          onClick={redirectToHome}
         >
           Voltar ao home
         </Button>
