86f5c4bd44a88ffc1a5939142999097971940d59
georgekaran <georgekaran12@gmail.com>
2020-11-10 22:08:46 -0300

    refactor: change Slider autoplay to 12s

--- src/presentation/components/Slider/Slider.tsx
+++ src/presentation/components/Slider/Slider.tsx
@@ -16,7 +16,7 @@ const defaultSettings: Settings = {
   speed: 1000,
   slidesToShow: 1,
   autoplay: true,
-  autoplaySpeed: 5000,
+  autoplaySpeed: 12000,
   pauseOnHover: true,
   slidesToScroll: 1
 }
