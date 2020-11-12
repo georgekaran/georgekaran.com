0d04e6578802c6f9f8784f725fdec8c1b95cd752
georgekaran <georgekaran12@gmail.com>
2020-11-12 17:10:27 -0300

    test(Projects): ensure renders with correct data

---
+++ src/test/pages/projects.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { screen } from '@testing-library/react'
+
+import { render } from '@/test/helpers'
+import ProjectsPage from '@/pages/projects'
+import messages from '@/main/config/messages'
+
+const message = messages.pt
+
+describe('Projects Page', () => {
+  it('should render with correct data', () => {
+    render(
+      <ProjectsPage />
+    )
+    expect(screen.getByText(message.projects)).toBeInTheDocument()
+    expect(screen.getByText(message.projects_description)).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /georgekaran.com/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Ticketing/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Survey App/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Ecoleta/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Star Wars UI/i })).toBeInTheDocument()
+  })
+})
