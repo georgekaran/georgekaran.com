3bcb1a474eec242b88ea3bc79c71074735e8336e
georgekaran <georgekaran12@gmail.com>
2020-11-12 16:57:28 -0300

    test(Contact): ensure renders with correct data

---
+++ src/test/pages/contact.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { screen } from '@testing-library/react'
+
+import { render } from '@/test/helpers'
+import ContactPage from '@/pages/contact'
+import messages from '@/main/config/messages'
+
+const message = messages.pt
+
+describe('Contact Page', () => {
+  it('should render with correct data', () => {
+    render(
+      <ContactPage />
+    )
+    expect(screen.getByText(message.contact_me)).toBeInTheDocument()
+    expect(screen.getByText(message.contact_me_description + ' 😀')).toBeInTheDocument()
+    expect(screen.getByRole('img')).toHaveAttribute('alt', message.contact_me_image_alt)
+  })
+})
