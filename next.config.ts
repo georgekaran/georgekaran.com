7ec96785f0b8c73516f54a3f045e44a39d429715
georgekaran <georgekaran12@gmail.com>
2026-07-11 14:23:46 -0300

    Upgrade Next.js to v16.3.0-canary.83 and enable `useTypeScriptCli` experimental flag

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    useTypeScriptCli: true,
+  }
 };
 
 export default nextConfig;
